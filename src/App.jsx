134c3431927fdd7509efb30e0e0195a6fe6fbbf9
James Michael <jomichael1994@gmail.com>
2024-01-19 15:13:33 +0000

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
+import CoreConcept from './components/CoreConcepts';
+import Header from './components/Header/Header';
+
 import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-const genRandomInt = (max) => {
-	return Math.floor(Math.random() * (max + 1));
-};
-
-const Header = () => {
-	const description = reactDescriptions[genRandomInt(2)];
-	return (
-		<header>
-			<img src={reactImg} alt="Stylized atom" />
-			<h1>React Essentials</h1>
-			<p>
-				{description} React concepts you will need for almost any app
-				you are going to build!
-			</p>
-		</header>
-	);
-};
-
-const CoreConcept = (props) => {
-	const { title, description, image } = props;
-	return (
-		<li>
-			<img src={image} alt={title} />
-			<h3>{title}</h3>
-			<p>{description}</p>
-		</li>
-	);
-};
 
 const App = () => {
 	return (
